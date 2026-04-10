45a2cbfc3bc9c4b2c3b59e22c1faa3b8d9a42af8
Abu Hanjala <abuhanjala656@gmail.com>
2026-04-10 17:33:06 +0530

    add delete and update feature

--- Day_04NoteApp/server.js
+++ Day_04NoteApp/server.js
@@ -1,49 +1,71 @@
-
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 const connectToDB = require("./src/db/db");
-const noteModel = require("./src/models/note.model")
+const noteModel = require("./src/models/note.model");
 const app = express();
 
 // middleware --
- app.use(express.json());
-
-
+app.use(express.json());
 /* ========================= Create API ================ */
 
 /*  notes create */
-app.post("/note" , async(req ,res)=>{
-     const {title , content} = req.body;
+app.post("/note", async (req, res) => {
+  const { title, content } = req.body;
 
-     await noteModel.create({
-          title,
-          content,
-     });
+  await noteModel.create({
+    title,
+    content,
+  });
 
-     res.json({
-       message: "Note create Successfully!",
-     })
-
+  res.json({
+    message: "Note create Successfully!",
+  });
 });
 
 // ---------------------------------------
 
 /*  Show the data */
-app.get("/note" , async(req,res)=>{
-     const notes = await noteModel.find();
-     res.json({
-          message:"Note fetch Successfully",
-          notes,
-     })
-})
+app.get("/note", async (req, res) => {
+  const notes = await noteModel.find();
+  res.json({
+    message: "Note fetch Successfully",
+    notes,
+  });
+});
 
+// ---------------------------------------
 
+/*  Delete method */
+app.delete("/note/:id", async (req, res) => {
+  const noteId = req.params.id;
+  await noteModel.findByIdAndDelete({
+    _id: noteId,
+  });
+  res.json({
+    message: "note Deleted Successfully !",
+  });
+});
 
+// ---------------------------------------
 
+/*  Update method */
 
+app.patch("/note/:id", async (req, res) => {
+  const noteId = req.params.id;
+  const { title } = req.body;
 
-
-
+  await noteModel.findOneAndUpdate(
+    {
+      _id: noteId,
+    },
+    {
+      title: title,
+    },
+  );
+    res.json({
+    message: "Note Updated Successfully."
+  })
+});
 
 // database call --
 connectToDB();
